22c05aa70df02f3cf53f192dc1a7fddf830bbcb4
Didou09 <didier.vezineT@gmail.com>
2021-06-28 21:58:09 +0200

    [#006] Initialize dparam, dvar and dfunc in Solver.__init__(), renamed dinput to dparam, refocused get_dparam() to returning dparam(), more generic get_summary(), added set_dvar() and _initialize_var(), TBF

--- _class.py
+++ _class.py
@@ -14,69 +14,136 @@ class Solver():
     """ Generic class
     """
 
-    _PARAMSET = 'v0'
+    _MODEL = 'v0'
+    _PARAMSET = _MODEL
+    _VARSET = 'GK'
 
     def __init__(self):
-        self.__dinput = {}
+        self.__dparam = {}
+        self.__dvar = {}
+        self.__dfunc = {}
 
-    def set_dinput(self, dinput=None, key=None, value=None):
-        """ Set the dict of input parameters (dinput) or a single param """
+    # ##############
+    # parameters
+
+    def set_dparam(self, dparam=None, key=None, value=None):
+        """ Set the dict of input parameters (dparam) or a single param """
 
         # If all None => set to self._PARAMSET
-        c0 = dinput is None and key is None and value is None
+        c0 = dparam is None and key is None and value is None
         if c0 is True:
-            dinput = self._PARAMSET
+            dparam = self._PARAMSET
 
-        # Check input: dinput xor (key, value)
+        # Check input: dparam xor (key, value)
         lc = [
-            dinput is not None,
+            dparam is not None,
             key is not None and value is not None,
         ]
         if np.sum(lc) != 1:
             lstr = [
                 '\t- {}: {}'.format(kk, vv)
                 for kk, vv in [
-                    ('dinput', dinput), ('key', key), ('value', value)
+                    ('dparam', dparam), ('key', key), ('value', value)
                 ]
             ]
             msg = (
-                "Please provide dinput xor (key, value)!\n"
+                "Please provide dparam xor (key, value)!\n"
                 + "You provided:\n"
                 + "\n".format(lstr)
             )
             raise Exception(msg)
 
-        # set dinput or update desired key
-        if dinput is not None:
-            self.__dinput = _class_checks.check_dinput(dinput=dinput)
+        # set dparam or update desired key
+        if dparam is not None:
+            self.__dparam, self.__paramset = _class_checks.check_dparam(
+                dparam=dparam,
+            )
         else:
-            if key not in self.__dinput.keys():
+            if key not in self.__dparam.keys():
                 msg = (
                     "key {} is not identified!\n".format(key)
-                    + "See get_dinput() method"
+                    + "See get_dparam() method"
                 )
                 raise Exception(msg)
-            self.__dinput[key]['value'] = value
+            self.__dparam[key]['value'] = value
+            self.__paramset = None
 
         # Update to check consistency
-        _class_checks.update_dinput(dinput=self.__dinput)
+        _class_checks.update_dparam(dparam=self.__dparam)
 
-    def get_dinput(self, verb=None, returnas=None):
+    def get_dparam(self, returnas=None):
         """ Return a copy of the input parameters dict
 
         Return as:
-            - False: do not return
             - dict: dict
-            - str: message to be printed
             - 'DataGFrame': a pandas DataFrame
             - np.ndarray: a dict of np.ndarrays
+        """
 
-        if verb = True the message is automatically printed
+        # check input
+        if returnas is None:
+            returnas = dict
+
+        # return
+        if returnas is dict:
+            # return a copy of the dict
+            return {k0: dict(v0) for k0, v0 in self.__dparam.items()}
+
+        elif returnas in [np.ndarray, 'DataFrame']:
+            lk = list(self.__dparam.keys())
+            out = {
+                'key': np.array(lk, dtype=str),
+                'value': np.array([
+                    np.nan if self.__dparam[k0]['value'] is None
+                    else self.__dparam[k0]['value']
+                    for k0 in lk
+                ]),
+                'com': np.array([self.__dparam[k0]['com'] for k0 in lk]),
+                'units': np.array([
+                    str(self.__dparam[k0]['units']) for k0 in lk
+                ]),
+            }
+            if returnas == 'DataFrame':
+                import pandas as pd
+                return pd.DataFrame.from_dict(out)
+            else:
+                return out
+
+
+    # ##############
+    # variables
+
+    def set_dvar(self, dvar=None):
+        """ Set the dict of variables """
+        if dvar is None:
+            dvar = self._VARSET
+        self.__dvar, self.__varset = _class_checks.check_dvar(dvar=dvar)
+        self._initialize_var()
+
+    def _initialize_var(self):
+        """ Create numpy arrays for each variable, full of nans """
+        nt = self.__dparam['Nt']['value']
+        nx = self.__dparam['Nx']['value']
+        for k0 in self.__dvar.keys():
+            self.__dvar[k0]['value'] = np.full((nt, nx), np.nan)
+
+    # ##############
+    # functions
+
+
+    # ##############
+    # show summary
+
+    def get_summary(self):
+        """
+        Print a str summary of the solver
 
         """
 
-        lcol = ['key', 'value', 'units', 'group', 'comment']
-        lar = [
+        # ----------
+        # parameters
+        col0 = ['parameter', 'value', 'units', 'group', 'comment']
+        ar0 = [
             tuple([
                 k0,
                 str(v0['value']),
@@ -84,48 +151,41 @@ class Solver():
                 v0['group'],
                 v0['com'],
             ])
-            for k0, v0 in self.__dinput.items()
+            for k0, v0 in self.__dparam.items()
         ]
-        msg = _utils._get_summary(
-            lar=[lar],
-            lcol=[lcol],
-            verb=verb,
-            returnas=str,
+
+        # ----------
+        # variables
+        col1 = ['variable', 'shape', 'units', 'comment']
+        ar1 = [
+            tuple([
+                k0,
+                'None' if v0['value'] is None else str(v0['value'].shape),
+                str(v0['units']),
+                v0['com'],
+            ])
+            for k0, v0 in self.__dvar.items()
+        ]
+
+        # ----------
+        # functions
+        col2 = ['function', 'comment']
+        ar2 = [
+            tuple([
+                k0,
+                v0['com'],
+            ])
+            for k0, v0 in self.__dfunc.items()
+        ]
+
+        # ----------
+        # format output
+        return _utils._get_summary(
+            lar=[ar0, ar1, ar2],
+            lcol=[col0, col1, col2],
+            verb=True,
+            returnas=False,
         )
-        if returnas is str:
-            return msg
-        elif returnas is dict:
-            # return a copy of the dict
-            return {k0: dict(v0) for k0, v0 in self.__dinput.items()}
-        elif returnas in [np.ndarray, 'DataFrame']:
-            lk = list(self.__dinput.keys())
-            out = {
-                'key': np.array(lk, dtype=str),
-                'value': np.array([
-                    np.nan if self.__dinput[k0]['value'] is None
-                    else self.__dinput[k0]['value']
-                    for k0 in lk
-                ]),
-                'com': np.array([self.__dinput[k0]['com'] for k0 in lk]),
-                'units': np.array([
-                    str(self.__dinput[k0]['units']) for k0 in lk
-                ]),
-            }
-            if returnas == 'DataFrame':
-                import pandas as pd
-                return pd.DataFrame.from_dict(out)
-
-    def initialize(self):
-        pass
-
-
-
-
-
-
-
-
-
 
 
 
