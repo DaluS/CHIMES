63b1bd186786ce935025a1202ee49e7aeb45f9da
VEZINET Didier <didier.vezinet@cea.fr>
2021-07-06 10:48:49 +0200

    [#006] Updated _def_parameters.py with fields taken from VariableDictionnary.py + added conformity checks

--- _def_parameters.py
+++ _def_parameters.py
@@ -35,6 +35,12 @@ _FLATTEN = True
 # ic['L'] = ic['lambda']*ic['N']
 # ic['W'] = ic['omega']*ic['a']
 
+_DALLOWED = {
+    'dimension': ['time', 'time rate', 'temperature rate'],
+    'type': ['intensive', 'extensive'],
+}
+
+
 
 # ##########################################
 # PARAMETERS PARAMETERS 
@@ -44,56 +50,92 @@ _DEF_PARAM = {
     # Numerical
     'Tmax': {
         'value': 100,
+        'name': 'time steps',
         'com': 'Duration of simulation',
-        'units': 't',
+        'dimension': 'time',
+        'units': 'y',
+        'type': None,
+        'symbol': None,
         'group': 'Numerical',
     },
     'Nx': {
         'value': 1,
+        'name': None,
         'com': 'Number of similar systems evolving in parrallel',
+        'dimension': None,
         'units': None,
+        'type': None,
+        'symbol': None,
         'group': 'Numerical',
     },
     'dt': {
         'value': 0.01,
-        'com': 'Timestep (fixed timestep method)',
+        'name': None,
+        'com': 'Time step (fixed timestep method)',
+        'dimension': 'time',
         'units': 't',
+        'type': None,
+        'symbol': None,
         'group': 'Numerical',
     },
     'Tstore': {
         'value': None,  # Dynamically allocated
+        'name': None,
         'com': 'Time between storages (if StorageMode=full, it goes to dt)',
+        'dimension': 'time',
         'units': None,
+        'type': None,
+        'symbol': None,
         'group': 'Numerical',
     },
     'Nt': {
         'value': None,  # Dynamically allocated
+        'name': None,
         'com': 'Number of temporal iteration',
+        'dimension': None,
         'units': None,
+        'type': None,
+        'symbol': None,
         'group': 'Numerical',
     },
     'Ns': {
         'value': None,  # Dynamically allocated
+        'name': None,
         'com': 'Number of elements stored',
+        'dimension': None,
         'units': None,
+        'type': None,
+        'symbol': None,
         'group': 'Numerical',
     },
     'verb': {
         'value': True,
+        'name': None,
         'com': 'flag indicating whether to print intermediate info',
+        'dimension': None,
         'units': None,
+        'type': None,
+        'symbol': None,
         'group': 'Numerical'
     },
     'storage': {
         'value': 'full',
+        'name': None,
         'com': 'flag indicating which time steps to store',
+        'dimension': None,
         'units': None,
+        'type': None,
+        'symbol': None,
         'group': 'Numerical'
     },
     'save': {
         'value': True,
+        'name': None,
         'com': 'flag indicating whether to save output data',
+        'dimension': None,
         'units': None,
+        'type': None,
+        'symbol': None,
         'group': 'Numerical'
     },
 
@@ -101,14 +143,22 @@ _DEF_PARAM = {
     # Population evolution 
     'beta': {
         'value': 0.025,
+        'name': None,
         'com': 'Rate of population growth',
+        'dimension': 'time rate',
         'units': 'y^-1',
+        'type': 'intensive',
+        'symbol': r'$beta$',
         'group': 'Population',
     },
     'alpha': {
         'value': 0.02,
+        'name': None,
         'com': 'Rate of productivity increase',
+        'dimension': 'time rate',
         'units': 'y^-1',
+        'type': 'intensive',
+        'symbol': r'$alpha$',
         'group': 'Population',
     },
 
@@ -116,8 +166,12 @@ _DEF_PARAM = {
     # Capital properties
     'delta': {
         'value': 0.005,
+        'name': None,
         'com': 'Rate of capital depletion',
+        'dimension': 'time rate',
         'units': 'y^-1',
+        'type': 'intensive',
+        'symbol': r'$\delta$',
         'group': 'Capital',
     },
 
@@ -125,26 +179,42 @@ _DEF_PARAM = {
     # Production
     'nu': {
         'value': 3,
+        'name': None,
         'com': 'Kapital to output ratio', #' !! IN CES its 1/A !!',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'\nu',
         'group': 'Production',
     },
     'eta': {
         'value': 1000,
+        'name': None,
         'com': '1/(1+substituability)', #CES parameter 
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': None,
         'group': 'Production',
     },
     'b': {
         'value': .5,
+        'name': None,
         'com': 'capital part of the production', #CES parameter 
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': None,
         'group': 'Production',
     },
     'z': {
         'value': 1,
+        'name': None,
         'com': 'Markup on salary estimation by employer',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': None,
         'group': 'Production',
     },
 
@@ -152,26 +222,42 @@ _DEF_PARAM = {
     # INTEREST / Price
     'r': {
         'value': .03,
+        'name': None,
         'com': 'Interest at the bank',
+        'dimension': 'time rate',
         'units': 'y^{-1}',
+        'type': 'intensive',
+        'symbol': None,
         'group': 'Prices',
     },
     'etaP': {
         'value': .192,
+        'name': None,
         'com': 'Typical rate for inflation',
+        'dimension': 'time rate',
         'units': 'y^{-1}',
+        'type': 'intensive',
+        'symbol': None,
         'group': 'Prices',
     },
     'muP': {
         'value': 1.3,
+        'name': None,
         'com': 'Mark-up of price',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': None,
         'group': 'Prices',
     },
     'gammaP': {
         'value': 1,
+        'name': None,
         'com': 'Money-illusion',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': None,
         'group': 'Prices',
     },
 
@@ -179,11 +265,12 @@ _DEF_PARAM = {
     # PHILIPS CURVE (employement-salary increase)
     'phinul': {
         'value': 0.04,
-        'com': (
-            'Unemployement rate at which there is no salary increase '
-            + 'with no inflation'
-        ),
+        'name': None,
+        'com': 'Unemployment rate that stops salary increase (no inflation)',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$\phi_0$',
         'group': 'Philips',
     },
 
@@ -191,20 +278,32 @@ _DEF_PARAM = {
     # KEEN INVESTMENT FUNCTION (profit-investment function)
     'k0': {
         'value': -0.0065,
+        'name': None,
         'com': '',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$k_0$',
         'group': 'Keen',
     },
     'k1': {
         'value': np.exp(-5),
+        'name': None,
         'com': '',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$k_1$',
         'group': 'Keen',
     },
     'k2': {
         'value': 20,
+        'name': None,
         'com': '',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$k_2$',
         'group': 'Keen',
     },
 
@@ -212,14 +311,22 @@ _DEF_PARAM = {
     # LINEAR DIVIDENT PROFITS 
     'div0': {
         'value': 0.138,
+        'name': None,
         'com': 'Part of GDP as dividends when pi=0',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$div_0$',
         'group': 'Dividends',
     },
     'div1': {
         'value': 0.473,
+        'name': None,
         'com': 'Slope',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$div_1$',
         'group': 'Dividends',
     },
 
@@ -227,26 +334,42 @@ _DEF_PARAM = {
     # Coupling Effets (EDP)
     'g1': {
         'value': .0,
+        'name': None,
         'com': 'GLOBAL EFFECTS OF LAMBDA (Mean field)',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$g_1$',
         'group': 'Coupling',
     },
     'g2': {
         'value': .00,
+        'name': None,
         'com': 'WITH NEIGHBORS EFFECTS OF LAMBDA (field)',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$g_2$',
         'group': 'Coupling',
     },
     'muI': {
         'value': 0.,
+        'name': None,
         'com': '',
+        'dimension': None,
         'units': "NOTDONEYET",
+        'type': 'intensive',
+        'symbol': r'$_mu_I$',
         'group': 'Coupling',
     },
     'muN': {
         'value': 0.,
+        'name': None,
         'com': '',
+        'dimension': None,
         'units': "NOTDONEYET",
+        'type': 'intensive',
+        'symbol': r'$\mu_N$',
         'group': 'Coupling',
     },
 
@@ -254,26 +377,42 @@ _DEF_PARAM = {
     # RELAXATION-BUFFER DYNAMICS
     'tauR': {
         'value': 2.0,
+        'name': None,
         'com': 'Typical time for recruitement',
+        'dimension': 'time',
         'units': 'y',
+        'type': 'intensive',
+        'symbol': r'$\tau_R$',
         'group': 'RelaxBuffer',
     },
     'tauF': {
         'value': 0.1,
+        'name': None,
         'com': 'Typical time for firing',
+        'dimension': 'time',
         'units': 'y',
+        'type': 'intensive',
+        'symbol': r'$\tau_F$',
         'group': 'RelaxBuffer',
     },
     'tauL': {
         'value': 2.,
+        'name': None,
         'com': 'Typical time for employement information',
+        'dimension': 'time',
         'units': 'y',
+        'type': 'intensive',
+        'symbol': r'$tau_L$',
         'group': 'RelaxBuffer',
     },
     'tauK': {
         'value': 2.,
+        'name': None,
         'com': 'Typical time on new capital integration',
+        'dimension': 'time',
         'units': 'y',
+        'type': 'intensive',
+        'symbol': r'$\tau_K$',
         'group': 'RelaxBuffer',
     },
 
@@ -281,26 +420,42 @@ _DEF_PARAM = {
     # GEMMES PARAMETERS
     'theta': {
         'value': 2.6,
+        'name': None,
         'com': 'Convexity on abattement cost function',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$\theta$',
         'group': 'Gemmes',
     },
     'dsigma': {
         'value': -0.001,
+        'name': None,
         'com': 'Variation rate of the growth of emission intensity',
+        'dimension': 'time rate',
         'units': 'y^{-1}',
+        'type': 'intensive',
+        'symbol': r'$\delta\sigma$',
         'group': 'Gemmes',
     },
     'dPBS': {
         'value': -0.005,
+        'name': None,
         'com': 'Growth rate of back-stop technology price',
+        'dimension': 'time rate',
         'units': 'y^{-1}',
+        'type': 'intensive',
+        'symbol': None,
         'group': 'Gemmes',
     },
     'dEland': {
         'value': -0.022,
+        'name': None,
         'com': 'Growth rate of land use change in CO2 emission',
+        'dimension': 'time rate',
         'units': 'y^{-1}',
+        'type': 'intensive',
+        'symbol': None,
         'group': 'Gemmes',
     },
 
@@ -309,32 +464,52 @@ _DEF_PARAM = {
     # D = 1 - (1 + p['pi1']*T + p['pi2']*T**2 + p['pi3']*T**p['zeta'] )**(-1)
     'pi1': {
         'value': 0.,
+        'name': None,
         'com': 'Linear temperature impact',
+        'dimension': 'temperature rate',
         'units': 'T^{-1}',
+        'type': 'intensive',
+        'symbol': r'$\pi_1$',
         'group': 'Damage',
     },
     'pi2': {
         'value': .00236,
+        'name': None,
         'com': 'Quadratic temperature impact',
+        'dimension': None,
         'units': 'T^{-2}',
+        'type': 'intensive',
+        'symbol': r'$\pi_2$',
         'group': 'Damage',
     },
     'pi3' : {
         'value': .00000507,
+        'name': None,
         'com': 'Weitzmann Damage temperature impact',
+        'dimension': None,
         'units': 'T^{-zeta}',
+        'type': 'intensive',
+        'symbol': r'$\pi_3$',
         'group': 'Damage',
     },
     'zeta': {
         'value': 6.754,
+        'name': None,
         'com': 'Weitzmann impact',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$\zeta$',
         'group': 'Damage',
     },
     'fk': {
         'value': 1./3.,   # Dangerous not to put points 1./3. (Python 2 vis 3)
+        'name': None,
         'com': 'Fraction of environmental damage',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$f_K$',
         'group': 'Damage',
     },
                                 # allocated to the stock of capital
@@ -342,62 +517,137 @@ _DEF_PARAM = {
     # Climate model
     'Phi12': {
         'value': .024,
+        'name': None,
         'com': 'Transfer of carbon from atmosphere to biosphere',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$\phi_{1\rightarrow2}$',
         'group': 'Climate',
     },
     'Phi23': {
         'value': .001,
+        'name': None,
         'com': 'Transfer from biosphere to stock',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': None,
         'group': 'Climate',
     },
     'C': {
         'value': 1/.098,
+        'name': None,
         'com': 'Heat capacity of fast-paced climate',
+        'dimension': None,
         'units': "SI",
+        'type': 'intensive',
+        'symbol': r'$\phi_{1\rightarrow2}$',
         'group': 'Climate',
     },
     'C0': {
         'value': 3.52,
+        'name': None,
         'com': 'Heat capacity of inertial component of climate',
+        'dimension': None,
         'units': "SI",
+        'type': 'intensive',
+        'symbol': r'$C_0$',
         'group': 'Climate',
     },
     'gammaHEAT': {
         'value': 0.0176,
+        'name': None,
         'com': 'Heat exchange coefficient between layer',
+        'dimension': None,
         'units': None,
+        'type': 'intensive',
+        'symbol': r'$\gamma_{heat}$',
         'group': 'Climate',
     },
     'Tsens': {
         'value': 3.1,
+        'name': None,
         'com': 'Climate sensitivity (deltaT/log2CO2)',
+        'dimension': None,
         'units': 'T',
+        'type': 'intensive',
+        'symbol': r'$T_{sens}$',
         'group': 'Climate',
     },
 
     'FexoMax': {
         'value': 0.7,
+        'name': None,
         'com': 'Maximal exougenous radiative forcing',
+        'dimension': None,
         'units': 'W M^{-2}',
+        'type': 'intensive',
+        'symbol': None,
         'group': 'Climate',
     },
     'F2CO2': {
         'value': 3.681,
+        'name': None,
         'com': 'doubling CO2 impact on forced radiations',
+        'dimension': None,
         'units': 'W/m2',
+        'type': 'intensive',
+        'symbol': r'$F^2_{CO2}$',
         'group': 'Climate',
     },
 
     'PopSat': {
         'value': 12,
+        'name': None,
         'com': 'Maximal population (billions)',
+        'dimension': None,
         'units': 'Humans',
+        'type': 'intensive',
+        'symbol': r'$N_{sat}$',
         'group': 'Population',
     },
 
 }
+
+
+# #############################################################################
+# #############################################################################
+#       _DEF_PARAM: Fill in default values and check conformity
+# #############################################################################
+
+_dfail = {}
+_lkeys = [
+    'value', 'name', 'com', 'dimension', 'units', 'type', 'symbol', 'group',
+]
+for k0, v0 in _DEF_PARAM.items():
+
+    # Check existence of keys
+    lout = [ss for ss in _lkeys if ss not in v0.keys()]
+    if len(lout) > 0:
+        _dfail[k0] = f"missing keys: {lout}"
+        continue
+
+    # If com if filled but not name, use com to fill name (and vice-versa)
+    if v0['name'] is None and v0['com'] is not None:
+        _DEF_PARAM[k0]['name'] = v0['com']
+    elif v0['name'] is not None and v0['com'] is None:
+        _DEF_PARAM[k0]['com'] = v0['name']
+
+    # Try to spot any typo / mistake
+    if v0['dimension'] not in _DALLOWED['dimension'] + [None]:
+        _dfail[k0] = f"Non-conform dimension! ({v0['dimension']})"
+    if v0['type'] not in _DALLOWED['type'] + [None]:
+        _dfail[k0] = f"Non-conform type! ({v0['type']})"
+
+
+if len(_dfail) > 0:
+    lstr = [f"\t- {k0}: {v0}" for k0, v0 in _dfail.items()]
+    msg = (
+        "The following non-conformities have been spotted:\n"
+        + "\n".join(lstr)
+    )
+    raise Exception(msg)
 
 
 # #############################################################################
