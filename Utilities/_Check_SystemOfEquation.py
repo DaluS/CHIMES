57fb4ca5b7123df88d9a323d186a013f882e1e28
DaluS <paul.valcke@gmail.com>
2021-07-08 09:32:00 -0400

    Commenting some unused files

--- Utilities/_Check_SystemOfEquation.py
+++ Utilities/_Check_SystemOfEquation.py
@@ -5,6 +5,7 @@ Created on Wed Jul  7 17:34:54 2021
 @author: Paul Valcke
 """
 
+"""
 FindTheRightOrderOfResolution():
 
 FindParametersInModel():
@@ -12,3 +13,4 @@ FindParametersInModel():
 CheckIfVariavblesArePrepared():
     
 CheckIfParametersArePrepared():
+"""
