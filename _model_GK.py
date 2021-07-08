abffe3eddb30aa2ea25b3d1ae3305fc7e8f01cb9
VEZINET Didier <didier.vezinet@cea.fr>
2021-07-08 16:16:49 +0200

    [#006] Added _model_GK.py, TBF

---
+++ _model_GK.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+
+
+
+
+
+_DVAR = {
+    'a': None,
+    'N': None,
+    'K': None,
+    'W': None,
+    'D': None,
+}
+
+
+_DPARAM = {
+    # Fixed-value parameters
+    'r': {'value': None},
+    'k0': {'value': None},
+    'k1': {'value': None},
+    'k2': {'value': None},
+    'alpha': {'value': None},
+    'delta': {'value': None},
+    'beta': {'value': None},
+    'nu': {'value': None},
+    'phi0': {'value': None},
+    'phi1': {'value': None},
+    # functions
+    'Y': {'value': None},
+    'L': {'value': None},
+    'Pi': {'value': None},
+    'lambda': {'value': None},
+    'omega': {'value': None},
+    'd': {'value': None},
+    'pi': {'value': None},
+    'philips': {'value': None},
+    'kappa': {'value': None},
+    'I': {'value': None},
+    'g': {'value': None},
+}
+
+
+
+
