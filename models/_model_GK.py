c6b2c59d50ab7747a3a3a51d9b1e9f1b9a340279
Didou09 <didier.vezineT@gmail.com>
2021-07-12 21:30:51 +0200

    [#202] Renamed value to func and added inital value for functions in _model_GK.py

--- models/_model_GK.py
+++ models/_model_GK.py
@@ -15,10 +15,23 @@ All parameters can have value:
 import numpy as np
 
 
+# ---------------------------
+# user-defined function order (optional)
+
+_FUNC_ORDER = None
+
+
+
+# ---------------------------
+# user-defined model
+# contains parameters and functions of various types
+
 _DPARAM = {
 
     # ---------
-    # Fixed-value parameters
+    # exogenous parameters
+    # can also have a time variation (just replace by a function of time)
+    # useful for studying the model's reaction to an exogenous shock
     'r': None,
     'k0': None,
     'k1': None,
@@ -32,90 +45,96 @@ _DPARAM = {
     # ---------
     # functions
 
-    # former variables
+    # differential equations (ode)
     'a': {
-        'value': lambda alpha=0, self=0: alpha * self,
+        'func': lambda alpha=0, itself=0: alpha * itself,
+        'initial': 0.1,
         'com': 'Exogenous technical progress as an exponential',
         'eqtype': 'ode',
     },
     'N': {
-        'value': lambda beta=0, self=0: beta*self,
+        'func': lambda beta=0, itself=0: beta*itself,
+        'initial': 1.,
         'com': 'Exogenous population as an exponential',
         'eqtype': 'ode',
     },
     'K': {
-        'value': lambda I=0, self=0, delta=0: I - self*delta,
+        'func': lambda I=0, itself=0, delta=0: I - itself*delta,
+        'initial': 0.1,
         'com' : 'Capital evolution from investment and depreciation',
         'eqtype': 'ode',
     },
     'W': {
-        'value': lambda self=0, philips=0: self * philips,
+        'func': lambda itself=0, philips=0: itself * philips,
+        'initial': 0.1,
         'com': 'Wage evolution through philips curve',
         'eqtype': 'ode',
     },
     'D': {
-        'value': lambda I=0, Pi=0: I - Pi,
+        'func': lambda I=0, Pi=0: I - Pi,
+        'initial': 0.1,
         'com' : 'Debt as Investment-Profit difference',
         'eqtype': 'ode',
     },
 
-    # Intermediary computed but not stored
+    # Intermediary functions (endogenous, computation intermediates)
     'Y': {
-        'value': lambda K=0, nu=1: K / nu,
+        'func': lambda K=0, nu=1: K / nu,
         'eqtype': 'intermediary',
     },
     'L': {
-        'value': lambda K=0, a=1, nu=1: K / (a * nu),
+        'func': lambda K=0, a=1, nu=1: K / (a * nu),
         'eqtype': 'intermediary',
     },
     'Pi': {
-        'value': lambda Y=0, W=0, L=0, r=0, D=0: Y - W*L - r*D,
+        'func': lambda Y=0, W=0, L=0, r=0, D=0: Y - W*L - r*D,
         'eqtype': 'intermediary',
     },
     'lambda': {
-        'value': lambda L=0, N=1: L / N,
+        'func': lambda L=0, N=1: L / N,
         'eqtype': 'intermediary',
     },
     'omega': {
-        'value': lambda W=0, L=0, Y=1: W * L / Y,
+        'func': lambda W=0, L=0, Y=1: W * L / Y,
         'eqtype': 'intermediary',
     },
     'philips': {
-        'value': lambda phi0=0,  phi1=0, lamb=0: -phi0 + phi1 / (1-lamb)**2,
+        'func': lambda phi0=0,  phi1=0, lamb=0: -phi0 + phi1 / (1-lamb)**2,
         'eqtype': 'intermediary',
     },
     'kappa': {
-        'value': lambda k0=0, k1=0, k2=0, Pi=0, Y=1: k0 + k1 * np.exp(k2*Pi/Y),
+        'func': lambda k0=0, k1=0, k2=0, Pi=0, Y=1: k0 + k1 * np.exp(k2*Pi/Y),
         'eqtype': 'intermediary',
     },
     'I': {
-        'value': lambda Y=0, kappa=0: Y * kappa,
+        'func': lambda Y=0, kappa=0: Y * kappa,
         'eqtype': 'intermediary',
     },
 
-    # auxiliary, not computed, but can be retrieved at the end
+    # auxiliary, not used for computation but for interpretation
+    # => typically computed at the end after the computation
     'g': {
-        'value': lambda omega=0, nu=1, delta=0: (1-omega) / nu - delta,
+        'func': lambda omega=0, nu=1, delta=0: (1-omega) / nu - delta,
         'eqtype': 'auxiliary',
     },
     'd': {
-        'value': lambda D=0, Y=1: D / Y,
+        'func': lambda D=0, Y=1: D / Y,
         'eqtype': 'auxiliary',
     },
     'pi': {
-        'value': lambda omega=0, r=0, d=0: 1 - omega - r*d,
+        'func': lambda omega=0, r=0, d=0: 1 - omega - r*d,
         'eqtype': 'auxiliary',
     },
     'i': {
-        'value': lambda Y=0: Y*0,
+        'func': lambda Y=0: Y*0,
         'eqtype': 'auxiliary',
     },
     'phi0': {
-        'value': lambda phinull=0: phinull / (1- phinull**2),
+        'func': lambda phinull=0: phinull / (1- phinull**2),
         'eqtype': 'auxiliary',
     },
     'phi1': {
-        'value': lambda phinull=0: phinull**3 / (1- phinull**2),
+        'func': lambda phinull=0: phinull**3 / (1- phinull**2),
         'eqtype': 'auxiliary',
     },
 }
