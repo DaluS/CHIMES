16a2b8353539db2c448dd83f1c44d80df158d762
DaluS <paul.valcke@gmail.com>
2021-09-20 15:00:15 -0400

    pgm named readded
    the library was called gemmes inside cockpit

--- Cockpit.py
+++ Cockpit.py
@@ -9,47 +9,58 @@ Contains all the possibilities of each _core interaction
 import os
 
 
-import gemmes as gem
-from gemmes import _plots as plots
+import pygemmes as pgm
+from pygemmes import _plots as plots
 
 
-_PATH_OUTPUT_REF = os.path.join('gemmes', 'tests', 'output_ref')
+_PATH_OUTPUT_REF = os.path.join('pygemmes', 'tests', 'output_ref')
 _MODEL = 'GK'
 
 
 # %% SHORT RUN ###############################################################
-hub = gem.Hub(_MODEL)
+hub = pgm.Hub(_MODEL)
 hub.run(verb=1.1)
 
 
 ##############################################################################
-
-
 # %% Information on available models
 
-dmodels = gem.get_available_models(
+dmodels = pgm.get_available_models(
     returnas=dict, details=False, verb=True,
 )
 
 # Get available solvers
-dsolvers = gem.get_available_solvers(
+dsolvers = pgm.get_available_solvers(
     returnas=dict, verb=True,
 )
 
 # %% Saved run available
-dout = gem.get_available_output(
+dout = pgm.get_available_output(
     path=_PATH_OUTPUT_REF, returnas=dict, verb=True,
 )
 
 
 # %% Load a model
 
-hub = gem.Hub(_MODEL)  # , preset='default')
+hub = pgm.Hub(_MODEL)  # , preset='default')
 # hub.load_preset('crisis')
 
 
 # %% Load a file
 # sol_load = _core._saveload.load(' ')
+
+
+# %% Change a parameter
+'''
+* To change a parameter :  `hub.set_dparam(key='alpha', value=0.01)`
+
+
+hub.set_dparam({'a' : [1,2,'lin']})
+hub.set_dparam({'a' : [1,2,'log'],
+                'alpha' : 0.03})
+hub.set_dparam({'nx': 100})
+hub.set_dparam({'a': [1,2,3]})
+'''
 
 # %% Runs
 
@@ -81,18 +92,6 @@ hub.dmisc
 allkey, allvars = hub.get_variables_compact(eqtype=None)
 
 Result = hub.get_dparam(returnas=dict)
-
-# %% Change a parameter
-'''
-* To change a parameter :  `hub.set_dparam(key='alpha', value=0.01)`
-
-
-hub.set_dparam({'a' : [1,2,'lin']})
-hub.set_dparam({'a' : [1,2,'log'],
-                'alpha' : 0.03})
-hub.set_dparam({'nx': 100})
-hub.set_dparam({'a': [1,2,3]})
-'''
 
 
 # %% DEEPER ANALYSIS
