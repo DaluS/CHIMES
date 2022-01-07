41b1b1062b3dcef40be66c6ff4e3c59d2903d472
Didou09 <didier.vezineT@gmail.com>
2022-01-07 14:56:31 +0100

    [#132] Putting the private pygemmes creation first in pgm.__init__.py

--- pygemmes/__init__.py
+++ pygemmes/__init__.py
@@ -2,13 +2,15 @@
 
 
 # Here we decide what the user will see
+from ._private_pygemmes import create_private_pygemmes
+
+# run private pygemmes creation, be conservative
+# has to be done before other imports!
+create_private_pygemmes(reset=False, reset_hard=False)
+
+
 from ._core import Hub
 from ._models import get_available_models
 from ._utilities._solvers import get_available_solvers
 from ._utilities._saveload import get_available_output, load
 from ._global_func import *
-from ._private_pygemmes import create_private_pygemmes
-
-
-# run private pygemmes creation, be conservative
-create_private_pygemmes(reset=False, reset_hard=False)
