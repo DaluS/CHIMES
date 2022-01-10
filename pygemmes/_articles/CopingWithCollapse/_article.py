6d8e30b755a501aef8781cc487c72ab329dd6fb5
VEZINET Didier <didier.vezinet@cea.fr>
2022-01-10 10:31:58 +0100

    [#134] CopyWithCollapse complete, with original figures, 3d example...

--- pygemmes/_articles/CopingWithCollapse/_article.py
+++ pygemmes/_articles/CopingWithCollapse/_article.py
@@ -5,6 +5,9 @@ import warnings
 
 
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import matplotlib.image as img
+from mpl_toolkits.mplot3d import Axes3D
 
 
 _PATH_HERE = os.path.dirname(__file__)
@@ -74,28 +77,63 @@ def plot_fig02(hub=None):
     # --------------------
     # Get data of interest
 
-    x = hub.dparam['lambda']['value'][:, 0]
-    y = hub.dparam['omega']['value'][:, 0]
+    kx, ky, kz = 'lambda', 'omega', 'g'
+    x = hub.dparam[kx]['value'][:, 0]
+    y = hub.dparam[ky]['value'][:, 0]
+    z = hub.dparam[kz]['value'][:, 0]
+
+    # --------------------
+    # Prepare plot parameters
+
+    x_label = hub.dparam[kx]['symbol'] + f" ({hub.dparam[kx]['units']})"
+    y_label = hub.dparam[ky]['symbol'] + f" ({hub.dparam[ky]['units']})"
+    z_label = hub.dparam[kz]['symbol'] + f" ({hub.dparam[kz]['units']})"
+
+    dmargin = {
+        'left': 0.1, 'right': 0.95,
+        'bottom': 0.1, 'top': 0.95,
+        'wspace': 0.1, 'hspace': 0.05,
+    }
+
+    # --------------------
+    # Load original figure
+
+    pfe_im = os.path.join(_PATH_HERE, 'fig02_Trajectory.png')
+    im = img.imread(pfe_im)
 
     # --------------------
     # prepare fig and axes
 
-    fig = plt.figure(figsize=(8, 5))
+    fig = plt.figure(figsize=(10, 5))
+    gs = gridspec.GridSpec(ncols=2, nrows=1, **dmargin)
 
-    ax0 = fig.add_axes([0.1, 0.1, 0.8, 0.8], aspect='equal')
-    ax0.set_xlabel('x (units)')
-    ax0.set_ylabel('y (units)')
+    # axe for reproduction
+    ax0 = fig.add_subplot(gs[0, 0], projection='3d')
+    ax0.set_xlabel(x_label)
+    ax0.set_ylabel(y_label)
+    ax0.set_zlabel(z_label)
     ax0.set_title('title', size=12, fontweight='bold')
 
-    dax = {'blabla': ax0}
+    # axe for original image
+    ax1 = fig.add_subplot(gs[0, 1], frameon=False)
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+
+    # store both in a dict
+    dax = {'reproduction': ax0, 'original': ax1}
 
     # ---------
     # plot data
 
-    kax = 'blabla'
+    kax = 'reproduction'
     if dax.get(kax) is not None:
         ax = dax[kax]
-        ax.plot(x, y)
+        ax.plot(x, y, z)
+
+    kax = 'original'
+    if dax.get(kax) is not None:
+        ax = dax[kax]
+        ax.imshow(im)
 
     return dax
 
@@ -106,9 +144,66 @@ def plot_fig02(hub=None):
 
 
 def plot_fig03(hub=None):
+    """ The objective here is to reproduce the original figure
+    """
 
+    # --------------------
+    # Get data of interest
 
-    dax = None
+    kx, ky = 'time', 'omega'
+    x = hub.dparam[kx]['value'][:, 0]
+    y = hub.dparam[ky]['value'][:, 0]
+
+    # --------------------
+    # Prepare plot parameters
+
+    x_label = hub.dparam[kx]['symbol'] + f" ({hub.dparam[kx]['units']})"
+    y_label = hub.dparam[ky]['symbol'] + f" ({hub.dparam[ky]['units']})"
+
+    dmargin = {
+        'left': 0.1, 'right': 0.95,
+        'bottom': 0.1, 'top': 0.95,
+        'wspace': 0.1, 'hspace': 0.05,
+    }
+
+    # --------------------
+    # Load original figure
+
+    pfe_im = os.path.join(_PATH_HERE, 'fig03_ProbabilityIntervals.png')
+    im = img.imread(pfe_im)
+
+    # --------------------
+    # prepare fig and axes
+
+    fig = plt.figure(figsize=(10, 5))
+    gs = gridspec.GridSpec(ncols=2, nrows=1, **dmargin)
+
+    # axe for reproduction
+    ax0 = fig.add_subplot(gs[0, 0])
+    ax0.set_xlabel(x_label)
+    ax0.set_ylabel(y_label)
+    ax0.set_title('title', size=12, fontweight='bold')
+
+    # axe for original image
+    ax1 = fig.add_subplot(gs[0, 1], frameon=False)
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+
+    # store both in a dict
+    dax = {'reproduction': ax0, 'original': ax1}
+
+    # ---------
+    # plot data
+
+    kax = 'reproduction'
+    if dax.get(kax) is not None:
+        ax = dax[kax]
+        ax.plot(x, y)
+
+    kax = 'original'
+    if dax.get(kax) is not None:
+        ax = dax[kax]
+        ax.imshow(im)
 
     return dax
 
@@ -119,9 +214,66 @@ def plot_fig03(hub=None):
 
 
 def plot_fig05(hub=None):
+    """ The objective here is to reproduce the original figure
+    """
 
+    # --------------------
+    # Get data of interest
 
-    dax = None
+    kx, ky = 'time', 'lambda'
+    x = hub.dparam[kx]['value'][:, 0]
+    y = hub.dparam[ky]['value'][:, 0]
+
+    # --------------------
+    # Prepare plot parameters
+
+    x_label = hub.dparam[kx]['symbol'] + f" ({hub.dparam[kx]['units']})"
+    y_label = hub.dparam[ky]['symbol'] + f" ({hub.dparam[ky]['units']})"
+
+    dmargin = {
+        'left': 0.1, 'right': 0.95,
+        'bottom': 0.1, 'top': 0.95,
+        'wspace': 0.1, 'hspace': 0.05,
+    }
+
+    # --------------------
+    # Load original figure
+
+    pfe_im = os.path.join(_PATH_HERE, 'fig03_ProbabilityIntervals.png')
+    im = img.imread(pfe_im)
+
+    # --------------------
+    # prepare fig and axes
+
+    fig = plt.figure(figsize=(10, 5))
+    gs = gridspec.GridSpec(ncols=2, nrows=1, **dmargin)
+
+    # axe for reproduction
+    ax0 = fig.add_subplot(gs[0, 0])
+    ax0.set_xlabel(x_label)
+    ax0.set_ylabel(y_label)
+    ax0.set_title('title', size=12, fontweight='bold')
+
+    # axe for original image
+    ax1 = fig.add_subplot(gs[0, 1], frameon=False)
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+
+    # store both in a dict
+    dax = {'reproduction': ax0, 'original': ax1}
+
+    # ---------
+    # plot data
+
+    kax = 'reproduction'
+    if dax.get(kax) is not None:
+        ax = dax[kax]
+        ax.plot(x, y)
+
+    kax = 'original'
+    if dax.get(kax) is not None:
+        ax = dax[kax]
+        ax.imshow(im)
 
     return dax
 
@@ -132,9 +284,66 @@ def plot_fig05(hub=None):
 
 
 def plot_fig06(hub=None):
+    """ The objective here is to reproduce the original figure
+    """
 
+    # --------------------
+    # Get data of interest
 
-    dax = None
+    kx, ky = 'lambda', 'K'
+    x = hub.dparam[kx]['value'][:, 0]
+    y = hub.dparam[ky]['value'][:, 0]
+
+    # --------------------
+    # Prepare plot parameters
+
+    x_label = hub.dparam[kx]['symbol'] + f" ({hub.dparam[kx]['units']})"
+    y_label = hub.dparam[ky]['symbol'] + f" ({hub.dparam[ky]['units']})"
+
+    dmargin = {
+        'left': 0.1, 'right': 0.95,
+        'bottom': 0.1, 'top': 0.95,
+        'wspace': 0.1, 'hspace': 0.05,
+    }
+
+    # --------------------
+    # Load original figure
+
+    pfe_im = os.path.join(_PATH_HERE, 'fig03_ProbabilityIntervals.png')
+    im = img.imread(pfe_im)
+
+    # --------------------
+    # prepare fig and axes
+
+    fig = plt.figure(figsize=(10, 5))
+    gs = gridspec.GridSpec(ncols=2, nrows=1, **dmargin)
+
+    # axe for reproduction
+    ax0 = fig.add_subplot(gs[0, 0])
+    ax0.set_xlabel(x_label)
+    ax0.set_ylabel(y_label)
+    ax0.set_title('title', size=12, fontweight='bold')
+
+    # axe for original image
+    ax1 = fig.add_subplot(gs[0, 1], frameon=False)
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+
+    # store both in a dict
+    dax = {'reproduction': ax0, 'original': ax1}
+
+    # ---------
+    # plot data
+
+    kax = 'reproduction'
+    if dax.get(kax) is not None:
+        ax = dax[kax]
+        ax.plot(x, y)
+
+    kax = 'original'
+    if dax.get(kax) is not None:
+        ax = dax[kax]
+        ax.imshow(im)
 
     return dax
 
