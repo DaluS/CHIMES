40de1bfa17d4b391ecfc76921e329d3603e064f1
DaluS <paul.valcke@gmail.com>
2022-01-28 17:45:57 -0500

    Init well written

--- pygemmes/__init__.py
+++ pygemmes/__init__.py
@@ -3,17 +3,15 @@
 
 # Here we decide what the user will see
 from ._private_pygemmes import create_private_pygemmes
-from ._models import get_available_models, get_available_functions
-from ._articles import get_available_articles, reproduce_article
-from ._toolbox import *
-from ._utilities._saveload import get_available_output, load
-from ._utilities._solvers import get_available_solvers
-from ._core import Hub
-
-
 # run private pygemmes creation, be conservative
 # has to be done before other imports!
 create_private_pygemmes(reset=False, reset_hard=False)
 
 
 # Here we decide what the user will see
+from ._models import get_available_models, get_available_functions
+from ._articles import get_available_articles, reproduce_article
+from ._toolbox import *
+from ._utilities._saveload import get_available_output, load
+from ._utilities._solvers import get_available_solvers
+from ._core import Hub
