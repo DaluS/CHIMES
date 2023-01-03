2080fbcc91ff3e0700dae6bb2aebaeff666dc761
DaluS <paul.valcke@gmail.com>
2023-01-03 18:09:18 -0500

    New file

---
+++ pygemmes/_utilities/_interface.py
@@ -0,0 +1,22 @@
+import ipywidgets as widgets
+from IPython.display import display,HTML,clear_output,Markdown
+import numpy as np
+import pandas as pd
+
+
+################# INITIALIZATION ############################## 
+default = '__EMPTY__' ## DEFAULT MODEL LOAD IN THE SYSTEM 
+style = {'description_width': 'initial'}
+pd.set_option('display.max_colwidth', None)
+AllModels = pgm.get_available_models(details=True)
+hub=pgm.Hub(default,verb=False)
+hub.run(verb=False)
+
+def pprint(ldf):
+    try :
+        ldf = ldf.style.set_properties(**{'text-align': 'left'})
+    except BaseException:
+        pass
+    return display(HTML(ldf.to_html().replace("\\n","<br>")))
+
+    
